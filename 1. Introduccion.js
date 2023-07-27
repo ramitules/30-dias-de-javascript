ce7d1d6427c1ac6e3279c935ab51b434f2a98ca7
ramiro <ramirotules@hotmail.com>
2023-07-26 22:59:13 -0300

    Introduccion

---
+++ 1. Introduccion.js
@@ -0,0 +1,1 @@
+console.log('Bienvenido a 30 dias de Javascript')
---
+++ helloworld.js
@@ -0,0 +1,56 @@
+console.log('Hola mundo!')
+
+let valorVacio = null
+console.log(typeof valorVacio)
+
+console.log(typeof 'Ramiro')
+
+// Comentario de linea
+
+/*
+Comentario
+de multiples
+lineas
+*/
+
+const gravedad = 9.81 // Constante
+let primerNombre = 'Ramiro', apellido = 'Tules'
+console.log(primerNombre, apellido)
+
+//1. Write a single line comment which says, comments can make code readable
+//comments can make code readable
+
+//2. Declare variables and assign string, boolean, undefined and null data types
+let s = String()
+let b = Boolean()
+let u = undefined
+let n = null
+
+//3. Use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof s, typeof b, typeof u, typeof n)
+
+//4. Declare four variables without assigning values
+let var1
+let var2
+let var3
+let var4
+
+//5. Declare four variables with assigned values
+let var5 = 'Ramiro'
+let var6 = 'Gaston'
+let var7 = 'Tules'
+let var8 = 25
+
+//6. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let name = 'Ramiro'
+let apellido1 = 'Tules'
+let estadoCivil = 'Soltero'
+let pais = 'Argentina'
+let edad = 25
+
+//7. Declare variables to store your first name, last name, marital status, country and age in a single line
+let name2 = 'Ramiro', apellido2 = 'Tules', estadoCivil2 = 'Soltero', pais2 = 'Argentina', edad2 = 25
+
+//8. Declare two variables myAge and yourAge and assign them initial values and log to the browser console
+let miEdad = 25, tuEdad = 99
+console.log('Mi edad:', miEdad, '. Tu edad:', tuEdad)
